618aede37ad5d19dac65dae561b19b716f11aff7
Md. Shaheen Hossain <rapidgrps@princegroup-bd.com>
2024-03-27 02:01:55 +0600

    hr_attendance and hr_attendance_gant 02

--- thrive/addons/hr_attendance/models/hr_attendance.py
+++ thrive/addons/hr_attendance/models/hr_attendance.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of thrive. See LICENSE file for full copyright and licensing details.
+# Part of Thrive. See LICENSE file for full copyright and licensing details.
 
 import pytz
 
@@ -264,7 +264,10 @@ class HrAttendance(models.Model):
             )[emp.resource_id.id]
             # Substract Global Leaves and Employee's Leaves
             leave_intervals = calendar._leave_intervals_batch(
-                start, stop, emp.resource_id, domain=self._get_overtime_leave_domain()
+                start, stop, emp.resource_id, domain=AND([
+                    self._get_overtime_leave_domain(),
+                    [('company_id', 'in', [False, emp.company_id.id])],
+                ])
             )
             expected_attendances -= leave_intervals[False] | leave_intervals[emp.resource_id.id]
 
